1917cf7c5ff52343049894bd3fca5a70c0db1b59
Daniel Lewis <daniellewis777@gmail.com>
2017-02-26 12:12:55 +0000

    Added docker build example

---
+++ Dockerfiles/build/files/server.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let app = express();
+
+app.use((req, res, next) => {
+  console.log(req.protocol.toUpperCase(), req.method, req.url);
+  return next();
+});
+
+app.use(express.static('public'));
+
+let listener = app.listen(8080, function () {
+  console.log('Server listening on port', listener.address().port);
+});
+
