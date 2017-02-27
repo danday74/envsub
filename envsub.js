737dfd5fd2438970eb3fb69eaa2033e048f453ab
Daniel Lewis <daniellewis777@gmail.com>
2017-02-27 21:42:25 +0000

    Allow local require

--- envsub.js
+++ envsub.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
-require('./index');
+
+const envsub = require('./index');
+
+if (process.argv.length < 3) {
+  console.error('input and output file args required');
+  process.exit(1);
+}
+
+let inFile = process.argv[2];
+let outFile = (process.argv.length > 3) ? process.argv[3] : null;
+
+envsub(inFile, outFile);
--- index.js
+++ index.js
@@ -3,24 +3,24 @@ let readFile = Promise.promisify(require('fs').readFile);
 let writeFile = Promise.promisify(require('fs').writeFile);
 let Handlebars = require('handlebars');
 
-// Ensure required args are given
-if (process.argv.length < 3) {
-  console.error('input and output file args required');
-  process.exit(1);
-}
-let inFile = process.argv[2];
-let outFile = (process.argv.length > 3) ? process.argv[3] : process.argv[2];
+let envsub = (inFile, outFile = null) => {
 
-readFile(inFile, 'utf8').then((inContents) => {
-  // Read the inFile and create a Handlebars template from its contents
-  let template = Handlebars.compile(inContents);
-  // Use env vars as the template data
-  let outContents = template(process.env);
-  // Write the result of the templating operation to the outFile
-  return writeFile(outFile, outContents);
-}).then(() => {
-  process.exit(0);
-}).catch((err) => {
-  console.error(err.message);
-  process.exit(1);
-});
+  outFile = outFile || inFile;
+
+  readFile(inFile, 'utf8').then((inContents) => {
+    // Read the inFile and create a Handlebars template from its contents
+    let template = Handlebars.compile(inContents);
+    // Use env vars as the template data
+    let outContents = template(process.env);
+    // Write the result of the templating operation to the outFile
+    return writeFile(outFile, outContents);
+  }).then(() => {
+    process.exit(0);
+  }).catch((err) => {
+    console.error(err.message);
+    process.exit(1);
+  });
+
+};
+
+module.exports = envsub;
